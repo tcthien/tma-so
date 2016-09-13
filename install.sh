2c1a249c2490ca66f5a6efa390b7ce3ea994bde0
Thien Tran <thientran1986@gmail.com>
2016-09-13 13:31:45 +0700

    Add code comment

--- install.sh
+++ install.sh
@@ -8,15 +8,20 @@ fi
 export TMA_SO_HOME=~/tma-so-software/
 export TMA_SO_TARGET_OS=$1
 
+# Check & create TMA_SO_HOME
+if [ -d "${TMA_SO_HOME}" ]; then
+    rm -rf ${TMA_SO_HOME}
+fi
 mkdir ${TMA_SO_HOME}
 chmod 777 ~/tma-so-software
-cd ${TMA_SO_HOME}
 
+# Check & clone exec source from github
+cd ${TMA_SO_HOME}
 if [ -d "${TMA_SO_HOME}/sources" ]; then
     rm -rf ${TMA_SO_HOME}/sources
 fi
-
 git clone https://github.com/tcthien/tma-so sources
-cd ${TMA_SO_HOME}/sources/scripts
 
+# Run setup
+cd ${TMA_SO_HOME}/sources/scripts
 ./setup ${1}
