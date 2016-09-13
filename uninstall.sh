ecd7d170ffff4b79990421fc41c206cbdc195086
Thien Tran <thientran1986@gmail.com>
2016-09-13 15:27:55 +0700

    Minor commit

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-if [ -z "${1}" ]; then
-    echo "Please include ENV Variable :<ubuntu (u) | raspbian (r)>"
-    exit
-fi
-
-export TMA_SO_HOME=~/tma-so-software/
-export TMA_SO_TARGET_OS=$1
-
-rm -rf ${TMA_SO_HOME}
-rm -rf ~/tmp/tma-so
+sudo rm -rf ~/tma-so-software/
+sudo rm -rf ~/tmp/tma-so
 
